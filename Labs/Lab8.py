42a57528385dcfce358157b7ed4c4aa00a06ce91
matishadow <matishadow@gmail.com>
2017-01-14 16:49:20 +0100

    Lab 8 addition

--- Labs/Lab8.py
+++ Labs/Lab8.py
@@ -1,5 +1,30 @@
 import math
 import cmath
+import Labs.Lab2
+
+
+def digits(k, n):
+    binary_number = [0] * n
+
+    is_positive = True
+    if k < 0:
+        is_positive = False
+        k *= -1
+
+    i = n - 1
+    while k != 0:
+        binary_number[i] = k % 2
+        k //= 2
+        i -= 1
+
+    if not is_positive:
+        for i in range(n):
+            binary_number[i] = 1 - binary_number[i]
+        number_to_add = [0] * n
+        number_to_add[n - 1] = 1
+        binary_number = Labs.Lab2.add(number_to_add, binary_number)
+
+    return binary_number
 
 
 def rec_fft(array):
